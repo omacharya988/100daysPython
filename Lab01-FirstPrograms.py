151144622f32f61153ad47af9905cb61b3b7815f
omacharya988 <85329171+omacharya988@users.noreply.github.com>
2024-01-09 21:14:18 -0700

    Add files via upload

---
+++ Lab01-FirstPrograms.py
@@ -0,0 +1,59 @@
+#Step 1
+print("Welcome")
+print("To")
+print("Python!")
+
+#Step 2
+print("Welcome\nTo\nPython!")
+
+#Step 3
+print("Welcome", "To", "Python!", sep="\n")
+
+#Step 4
+print("Welcome",end=" ")
+print("To",end=" ")
+print("Python!")
+
+#Step 5
+print("My Favorite Word is \"Python\".")
+
+#Step 6
+print("If I have")
+print("A6 in Hex that equals")
+print(0xA6,"in Decimal.")
+
+#Step 7
+print("If I have")
+print("75 in Octal that equals")
+print(0o75,"in Decimal.")
+
+#Step 8
+print("If I have")
+print("11011101 in Binary that equals")
+print(0b11011101,"in Decimal.")
+
+#Step 9
+print("If you were to multiply")
+print("A1 by 2B in Hex that equals")
+print(0xA1*0x2B,"in Decimal.")
+
+#Step 10
+print("12.5 * 7.35 =",12.5 * 7.35)
+
+#Step 11
+print("If the octet subnet mask is 11100000 it")
+print("would be",0b11100000,"in Decimal.")
+
+#Step 12
+print("If you purchased 250 worth of electronics, the total would be $", 250*1.13,"after 13% sales tax.")
+
+#Step 13
+print("Fanshawe "*3)
+
+#Step 14
+print("2 to the power of 5 is ",2**5,".",sep="")
+
+
+
+
+
